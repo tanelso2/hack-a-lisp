21eb1dc5f546a9a3d2ffa4175ab36cd6621f21d7
Thomas Nelson <tanelso2@gmail.com>
2025-01-09 19:42:06 -0600

    Version 0.3.1-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.clojars.tanelso2/hack-a-lisp "0.3.0"
+(defproject org.clojars.tanelso2/hack-a-lisp "0.3.1-SNAPSHOT"
   :description "Hack some lisps together, exploiting Clojure's read function to great effect."
   :url "http://github.com/tanelso2/hack-a-lisp"
   :license {:name "The Unlicense"
