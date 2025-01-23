8576857a96f10cd011171ec273080c6f6238c218
Thomas Nelson <tanelso2@gmail.com>
2025-01-22 19:58:51 -0600

    Expand on terraform-test

--- test/hack_a_lisp/langs/terraform_test.clj
+++ test/hack_a_lisp/langs/terraform_test.clj
@@ -2,7 +2,9 @@
   (:require
     [hack-a-lisp.langs.terraform :refer :all]
     [clojure.test :refer :all]
-    [clj-toolbox.test-utils :refer :all])
+    [clj-toolbox.test-utils :refer :all]
+    [clojure.pprint :as pprint]
+    [clj-toolbox.string-tools :refer [box-trim]])
   (:import
     [java.lang AssertionError]))
 
@@ -32,11 +34,41 @@
   (seq ['(:resource ["foo" "bar1"])
         '(:resource ["foo" "bar2"])]) "resource \"foo\" \"bar1\" {}\n\nresource \"foo\" \"bar2\" {}"
   ; Nonsense inputs
-  (Exception. "Hello") (thrown? Exception))
-
-(defntest-1 vars-evaluate
-  '{ecr_repos ["foo" "bar"]} "ecr_repos = [ \"foo\", \"bar\" ]"
-  '{foo 1 bar 2} "foo = 1\n\nbar = 2"
-  (seq ['{foo 1}]) "foo = 1"
-  (seq [1]) (thrown? AssertionError)
-  1 (thrown-with-msg? Exception #"should only consist of a single map"))
+  (Exception. "Hello") (thrown? Exception)
+  ;; Longform inputs
+  '(:module ["ecr"]
+     source "./modules/ecr/"
+     for_each var.ecr_repos
+     name each.key
+     mutable each.value.mutable
+     scan_on_push each.value.scan_on_push
+     policy (if (not= each.value.policy_file null)
+                (file (join "/" ["."
+                                 "resources"
+                                 "policies"
+                                 each.value.policy_file]))
+                null))
+  (box-trim "
+             module \"ecr\" {
+               source = \"./modules/ecr/\"
+               for_each = var.ecr_repos
+               name = each.key
+               mutable = each.value.mutable
+               scan_on_push = each.value.scan_on_push
+               policy = each.value.policy_file != null ? file(join(\"/\", [ \".\", \"resources\", \"policies\", each.value.policy_file ])) : null
+             }
+            ")
+  '(:terraform []
+      (:backend ["s3"]
+        bucket "tanelso2-terraform-state"
+        key "this.state"
+        region "us-east-2"))
+  (box-trim "
+             terraform {
+               backend \"s3\" {
+                 bucket = \"tanelso2-terraform-state\"
+                 key = \"this.state\"
+                 region = \"us-east-2\"
+               }
+             }
+            "))
